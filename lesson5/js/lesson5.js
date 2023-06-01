1a27901b7607e12cf36c93dbfb0cfae2d9fadc30
Hardeep <deshardeep@gmail.com>
2023-06-01 11:17:04 +0530

    JavaScript Operators and Expressions

---
+++ lesson5/js/lesson5.js
@@ -0,0 +1,75 @@
+console.log("Oprators and Expressions in Javascript 👇👇👇");
+
+let firstValue = 15;
+let secondValue = 4;
+
+// Arithimatic oprator
+console.log("Arithimatic Oprators 👇👇👇");
+
+console.log("firstValue + secondValue = ", firstValue + secondValue); // addition
+console.log("firstValue - secondValue = ", firstValue + secondValue); // Substration
+console.log("firstValue * secondValue = ", firstValue + secondValue); // Multiplication 
+console.log("firstValue / secondValue = ", firstValue + secondValue); // Divsion
+
+console.log("firstValue % secondValue = ", firstValue % secondValue); // Modulas
+console.log("firstValue ** secondValue = ", firstValue ** secondValue); // Expmantion
+
+console.log("firstValue++ = ", ++firstValue);  //  value= 16 // Incerement oprator
+console.log("firstValue++ = ", firstValue++); // increment  oprator
+console.log("firstValue-- = ", firstValue--); // Decrement oprator
+
+// Assigment Operators
+console.log("Assigment Oprators 👇👇👇");
+
+let assigment = 5;
+assigment += 1;
+console.log(" Plus equalto Oprators 👇👇👇");
+console.log(assigment);
+
+console.log(" Minus equalto Oprators 👇👇👇");
+assigment -= 1;
+console.log(assigment);
+
+console.log(" Multiplye equalto Oprators 👇👇👇");
+assigment *= 1;
+console.log(assigment);
+
+console.log(" Divison equalto Oprators 👇👇👇");
+assigment /= 1;
+console.log(assigment);
+
+// Comparsion Oprators 
+
+console.log("Comparsion Oprators 👇👇👇");
+
+let comp1 = 6;
+let comp2 = "6";
+
+console.log("comp1 == comp2". comp1 == comp2);
+console.log("comp1 != comp2". comp1 != comp2);
+console.log("comp1 === comp2". comp1 === comp2);
+console.log("comp1 !== comp2". comp1 === comp2);
+console.log("comp1 >= comp2". comp1 >= comp2);
+console.log("comp1 <= comp2". comp1 <= comp2);
+
+///Logical Oprators
+console.log("Logical Oprators 👇👇👇");
+
+let x = 5;
+let y = 6;
+
+console.log(x<y && x==y);
+console.log(x<y || x==y);
+console.log(!false);
+console.log(!true);
+
+
+
+
+
+
+
+
+
+
+
