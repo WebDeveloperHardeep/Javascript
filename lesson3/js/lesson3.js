56adae08a92b35a2734c1a94a663cbb0bd65a7e9
Hardeep <deshardeep@gmail.com>
2023-06-01 11:14:16 +0530

    Primitives and Objects in JavaScript

---
+++ lesson3/js/lesson3.js
@@ -0,0 +1,25 @@
+console.log("Primitves and Objects in javascript");
+// nn bb ss U
+
+let a1 = null;
+let b1 = 345;
+let c1 = true; // can also be false
+let d1 = BigInt("567") + BigInt("3");
+let e1 = "harry";
+let f1 = Symbol("i am a nice symbol");
+let g1
+
+console.log(a1, b1, c1, d1, e1, f1, g1);
+console.log(typeof c)
+
+// Object in js
+// Non Primitves Data types
+console.log("Non Primitves Data types");
+const item ={
+    "Harry": true,
+    "Shubh": false,
+    "Lavish": 67,
+    "Rohan": undefined
+}
+
+console.log(item['Lavish']);
