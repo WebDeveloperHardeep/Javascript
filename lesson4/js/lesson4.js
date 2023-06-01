fcf779b5a20f1ac0623ae959633848f56943b79f
Hardeep <deshardeep@gmail.com>
2023-06-01 11:15:50 +0530

    JavaScript Chapter 1 - Practice Set

---
+++ lesson4/js/lesson4.js
@@ -0,0 +1,27 @@
+console.log("Chapter 1 pratice set 👇👇👇")
+
+let a_1 ="harry";
+let b_1 = 6;
+console.log( a + b);
+
+// Chapter 1 - Q2
+console.log(typeof (a+b));
+// Chapter 1 - Q3 
+const a11 = {
+    name:"harry",
+    section: 1,
+    isPrincipal: false
+}
+// a1 = 45;
+// Chapter 1 - Q4
+a11['friend'] = "Shubham";
+a11['name'] = "Lavish";
+console.log(a11)
+
+// Chapter 1 - Q5
+const dict = {
+    apprecicate : "Recognize the full worth of",
+    atraxia : "a state of freedom from emotional and anxiety",
+    yakka : "Work, especially hard work"
+}
+console.log('yakkaa');
