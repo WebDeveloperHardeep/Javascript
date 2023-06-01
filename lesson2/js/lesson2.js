ab69686a28bc98355bb9593af3ad992f69557322
Hardeep <deshardeep@gmail.com>
2023-06-01 11:12:05 +0530

    const, let and var in JavaScript

---
+++ lesson2/js/lesson2.js
@@ -0,0 +1,15 @@
+        // Const , let and var in java script //
+        console.log("javascript tutoral 3. var , let and const");
+        // var a = 45;
+        // var a = "p";
+        let b = "harry"
+        const author = "harry";
+        // author = 5 /// Throws an error beacuse constant can not be changed
+        b = 4;
+        var c = null;
+        var d = undefined;
+        {
+            let b = 'this';
+            console.log(b);
+        }
+        console.log(b);
