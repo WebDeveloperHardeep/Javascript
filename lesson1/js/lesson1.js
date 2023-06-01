344217b1c4cea37e086e129317fe1be1b4874975
Hardeep <deshardeep@gmail.com>
2023-06-01 11:10:12 +0530

    Variables In Javascript

---
+++ lesson1/js/lesson1.js
@@ -0,0 +1,9 @@
+console.log("This Repl Contains code for the ultimate Course video no 2")
+
+let a = 67; /// a contains 67
+console.log(a);
+a = "harry";
+console.log(a);
+
+// let 8harry = 7; /// Not allowed this will throw an error
+// let var = 7;  /// Not allowed this will throw an error
