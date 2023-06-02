3fa8ccd213c4e469155ef55103150133c04cea97
Hardeep <deshardeep@gmail.com>
2023-06-02 18:51:10 +0530

    Loops Practice again

---
+++ lesson8/js/loopspractice.js
@@ -0,0 +1,31 @@
+// Program is add First a Natural Numbers
+// Noraml For Loop 
+let sum = 0
+let n = prompt("Enter The Value of n")
+n = Number.parseInt(n)
+
+for(let i = 0; i<n; i++){
+    // console.log(i+1);
+    sum += (i+1);
+    console.log(i+1, "+");
+}
+console.log("Sum of First" + n + "natural number is" + sum);
+console.log(i);
+
+// for In Loops
+ let obj ={
+    harry: 56,
+    hardeep: 90,
+    Gyan: 56,
+    Gaganjot: 57,
+    Gurdeep: 23,
+ }
+for(let a in obj){
+    console.log(" Marks of " + a + " Are " + obj[a]);
+}
+
+// for of Loops
+
+for(let b of "harry"){
+    console.log(b);
+}
