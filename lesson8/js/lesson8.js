fcaa447d4335e7733fe805c8369f1aff83d44e01
Hardeep <deshardeep@gmail.com>
2023-06-02 09:11:53 +0530

    undo Commet

--- lesson8/js/lesson8.js
+++ lesson8/js/lesson8.js
@@ -1,15 +1,16 @@
 // Program is add First a Natural Numbers
 // Noraml For Loop 
-// let sum = 0
-// let n = prompt("Enter The Value of n")
-// n = Number.parseInt(n)
+let sum = 0
+let n = prompt("Enter The Value of n")
+n = Number.parseInt(n)
 
-// for(let i = 0; i<n; i++){
-//     // console.log(i+1);
-//     sum += (i+1);
-//     console.log(i+1, "+");
-// }
-// console.log("Sum of First" + n + "natural number is" + sum);
+for(let i = 0; i<n; i++){
+    // console.log(i+1);
+    sum += (i+1);
+    console.log(i+1, "+");
+}
+console.log("Sum of First" + n + "natural number is" + sum);
+console.log(i);
 
 // for In Loops
  let obj ={
