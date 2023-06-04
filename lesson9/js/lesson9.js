34e10b754f2469641e5f4dc410894224a1d594e8
Hardeep <deshardeep@gmail.com>
2023-06-04 18:15:46 +0530

    Using Do while loop Js

--- lesson9/js/lesson9.js
+++ lesson9/js/lesson9.js
@@ -1,9 +1,19 @@
 // Topic - While loops
+// let n = prompt("Enter The Value of a");
+// n = Number.parseInt(n);
+
+// let i = 0;
+// while(i<n){
+//     console.log(i);
+//     i++;
+// }
+
+// Topic Do while loop
 let n = prompt("Enter The Value of a");
 n = Number.parseInt(n);
 
-let i = 0;
-while(i<n){
+let i = 10;
+do{
     console.log(i);
     i++;
-}
+}while(i<n)
