ddc1998b61f9710245f1114263c2f803175c7c5c
Hardeep <deshardeep@gmail.com>
2023-06-06 18:05:31 +0530

    Using String Js

---
+++ lesson12/js/lesson12.js
@@ -0,0 +1,29 @@
+/// Intro To Strings JS
+
+let name = "harry"
+// console.log(name.length);
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+console.log(name[3])
+console.log(name[4])
+// console.log(name[5])
+
+let friend = 'Prakash'
+console.log(friend)
+// let myFriend = "Shubh'  // DO not do this
+// console.log(myfriend)
+
+// Templete Litrals
+
+let boy1 = "promod"
+let boy2 = "Nikhil"
+// Nikhil is friend of promoud
+
+let sentense = `${boy1} is a friend of ${boy2}`
+console.log(sentense)
+
+//Escape Sequencee Character
+
+let fruit = "Bana\"na"
+console.log(fruit)
