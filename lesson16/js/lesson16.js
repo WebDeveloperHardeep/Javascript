5e827e41269e0c023e5d91483989d0dc0aae458d
Hardeep <deshardeep@gmail.com>
2023-06-08 19:04:27 +0530

    using array method in js

---
+++ lesson16/js/lesson16.js
@@ -0,0 +1,19 @@
+
+// Aray Methods
+
+let num1 = [1, 2, 3, 5, 23, 4]
+let a = num1.toString() // b is change into string
+console.log(a, typeof a)
+let c = num1.join(" and ")
+console.log(c, typeof c)
+// let r = num1.pop() // pop returns the popped element
+// console.log(num1, 4)
+
+// let r = num1.push(56) // push return the new array length
+// console.log(num1, r)
+
+// let r =num1.shift() // Removes an element from the start of tha array
+// console.log(num1, r)
+
+let r =num1.unshift() // Return new array length
+console.log(num1, r)
