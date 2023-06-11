d23c110c747693b299708cb847f7d6cbbc112d0b
Hardeep <deshardeep@gmail.com>
2023-06-11 19:01:07 +0530

    using arrays pratice set

---
+++ lesson20/js/lesson20.js
@@ -0,0 +1,55 @@
+   // JavaScript Chapter 5 - Practice Set on Arrays
+
+//    pratice problem
+// let arr = [1, 2, 3, 4, 5, 6, 7, 83]
+// let a = prompt("Enter a number")
+
+// a = Number.parseInt(a)
+// arr.push(a)
+// console.log(arr)
+
+// practice problem 2
+
+// let arr =  [1, 2, 3, 4, 5, 6, 7, 83]
+// let a;
+// do{
+//     let a = prompt("Enter a number")
+//     a = Number.parseInt(a)
+//     arr.push(a)
+//     console.log(arr)
+// }while(a!=0);
+// // let a = prompt("Enter a number")
+// // a = Number.parseInt(a)
+// // arr.push(a)
+// console.log(arr)
+
+// Pratice Problem 3
+
+// let arr = [1, 2, 30, 4, 50, 6, 7,83, 670]
+// let n = arr.filter((x)=>{
+//     return x%10
+
+// })
+// console.log(n)
+
+// pratice problem 4
+
+
+// let arr = [1, 2, 30, 4, 50, 6, 7,83, 670]
+// let n = arr.map((x)=>{
+//     return x*x
+
+// })
+// console.log(n)
+
+
+// pratice problem 5
+
+
+let arr = [1, 2, 3, 4, 5]
+let n = arr.reduce((x1, x2)=>{
+    return x1*x2
+
+})
+console.log(n)
+
