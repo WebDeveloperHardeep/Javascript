0aabd3bcb944fe388aba9c0a072a7c46cbcd94f6
Hardeep <deshardeep@gmail.com>
2023-06-29 14:55:46 +0530

    Using Dom,Bom & Window

---
+++ lesson24/js/lesson24.js
@@ -0,0 +1,10 @@
+// 1.Window Object
+// 2. bom = Browser object model
+// 3. dom = Docement object model
+
+window.console.log(window)
+console.log(document.body) // dom example
+
+document.body.style.background = "Yellow" // For Apply any style
+
+alert("Yuu are doning something wrong")
