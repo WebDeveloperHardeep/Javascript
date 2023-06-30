4fd7915fc793ec0be0ec696d7540bae8fa66520f
Hardeep <deshardeep@gmail.com>
2023-06-30 19:21:28 +0530

    using innerHTML,outerHTML and other propties in js

---
+++ lesson33/js/lesson33.js
@@ -0,0 +1,18 @@
+// let x = console.log(document.getElementsByTagName("span")[0])
+// let y = console.dir(document.getElementsByTagName("span")[0])
+
+// console.log(document.body.firstChild.nodeName)
+// console.log(document.body.firstElementChild.nodeName)
+
+// first.innerHTML
+// first.InnerHTML = "<i>I Am Italic</i>"
+// first.outerHTML
+// first.outerHTML ="<div>Hey</div>"
+
+// Text Content
+// document.body.firstChild.data
+// console.log(document.body.textContent)
+
+// the HIddden Property
+// first.hidden = false;
+// first.style.display = "none"
