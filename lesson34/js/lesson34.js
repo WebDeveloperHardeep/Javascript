1f51b2b4e08baab1c750bd5305d6c131e9a25078
Hardeep <deshardeep@gmail.com>
2023-07-01 10:06:54 +0530

    Using HTML Attributes and their methods in js

---
+++ lesson34/js/lesson34.js
@@ -0,0 +1,22 @@
+
+let first = document.getElementById("first")
+let a = first.getAttribute("class") // method 1
+// console.log(a)
+
+// console.log(first.hasAttribute("class")) // True // method 2
+// console.log(first.hasAttribute("style")) // False
+
+// // first.setAttribute("hidden", "true") // method 3
+
+// first.setAttribute("class", " true Sachin")
+
+// // method 4
+// // first.removeAttribute("class")
+
+// // method 5
+// console.log(first.attributes)
+
+// method 6
+// console.log(first.dateset)
+// console.log(first.dateset.game)
+// console.log(first.dateset.player)
