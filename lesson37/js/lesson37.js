77db7de6cd826424ac112c8f5280e35e62fd245e
Hardeep <deshardeep@gmail.com>
2023-07-01 14:10:04 +0530

    Using classes,className,classList in js

---
+++ lesson37/js/lesson37.js
@@ -0,0 +1,9 @@
+first.className = "text-black red"
+// method 1
+first.classList.remove = ("red") // for remove any class
+// method 2
+first.classList.add("yellow")
+// method 3
+first.classList.toggle("red")
+// method 4
+first.classList.contains("red")
