50c6b0a2687a9b44b858015e6d1c044d9a9548d4
Hardeep <deshardeep@gmail.com>
2023-07-01 13:40:00 +0530

    using  insrtAdjacentHTML,Element,text in js

---
+++ lesson36/js/lesson36.js
@@ -0,0 +1,14 @@
+// insert adjacent Html 
+// insert adjacent Element
+// insert adjacent Text
+// method 1
+// first.insertAdjacentHTML("beforebegin", '<div class="test">Beforebegin</div>');
+// method 2
+// first.insertAdjacentHTML("beforeend", '<div class="test">Beforebegin</div>');
+// method 3
+// first.insertAdjacentHTML("afterbegin", '<div class="test">afterbegin</div>');
+// method 4
+// first.insertAdjacentHTML("afterend", '<div class="test">afterend</div>');
+
+// method 5
+first.remove()
