ce50b15b49a62e5746d2dbf98fdceb5ae4116371
Hardeep <deshardeep@gmail.com>
2023-08-07 16:04:27 +0530

    using Error Handling: try and catch in js

---
+++ lesson51/js/lesson51.js
@@ -0,0 +1,40 @@
+
+setTimeout(()=>{
+    console.log("Hacking Wifi..... please wait....")
+
+},2000)
+
+// console.log(rahul) it is showing error beacuse it is wrong place for printing 
+// let solve with that solution
+ 
+// For Error Handling 
+try{
+    console.log(rahul)
+ }
+ catch(err){
+    console.log("Balle Balle" + err)
+ }
+ // For Error Handling
+
+setTimeout(()=>{
+    console.log("Fetching username and password.... please wait....")
+
+},2000)
+
+
+setTimeout(()=>{
+    console.log("Hacking Rahul's facebook ..... please wait....")
+
+},2000)
+
+
+
+setTimeout(()=>{
+    console.log("Username and password of rahul (+853892647639) fetched..... please wait....")
+
+},2000)
+
+
+
+
+
