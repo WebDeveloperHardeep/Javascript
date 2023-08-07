1af1031b9692484754769b2d280f315824c32291
Hardeep <deshardeep@gmail.com>
2023-08-07 16:30:57 +0530

    Using The Finally Clause in js

---
+++ lesson53/js/lesson53.js
@@ -0,0 +1,21 @@
+const f = ()  =>{
+
+try {
+    let a = 0
+    // console.log(program)
+    console.log("Program ran Successfully")
+    return
+}
+catch(err){
+    console.log("This is an error")
+    // console.log(p)
+}
+finally{
+    console.log("I Am Good Boy")
+    // close the file
+    // exit the loop
+    // write to the log file
+}
+}
+
+f()
