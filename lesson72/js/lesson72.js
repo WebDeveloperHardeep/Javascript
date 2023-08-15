2cb95566d14ce5d3039651581d7b0db4b227921e
Hardeep <deshardeep@gmail.com>
2023-08-15 17:05:02 +0530

    Using Hosting in js

---
+++ lesson72/js/lesson72.js
@@ -0,0 +1,11 @@
+// Following two lines will run sucessfully due to hostitng
+
+// let a = 9;
+console.log(a)
+great()
+function great(){
+    console.log("Good Morning")
+}
+
+var a = 9;// Declaration hosited to top but initalaization is not
+console.log(a)
